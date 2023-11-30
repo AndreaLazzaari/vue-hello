57eedb1a4839013c7037177c93a735008ca98e94
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-30 14:14:24 +0100

    esercizio completato con bonus

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      title: 'L\' inizio di Vue! Sarà la mia rovina?',
+      img:'https://www.gamesvillage.it/wp-content/uploads/2023/11/Destiny-2-scaled.jpg',
     }
   }
 }).mount('#app')
